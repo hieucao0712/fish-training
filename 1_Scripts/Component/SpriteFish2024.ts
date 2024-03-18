98c748311c6b5e0d6f2a34ffc04b03ddb5d1146f
Hieu Cao <hieu.cao@enotion.io>
2024-03-18 14:31:58 +0700

    create spine fish and sprite fish

---
+++ 1_Scripts/Component/SpriteFish2024.ts
@@ -0,0 +1,49 @@
+import { _decorator, Animation, Color, Sprite } from 'cc';
+import { gfBaseFish } from '../../../../cc-common/cc30-fishbase/Scripts/Components/Fishes/gfBaseFish';
+const { ccclass, property } = _decorator;
+
+@ccclass('SpriteFish2024')
+export class SpriteFish2024 extends gfBaseFish {
+    @property(Animation)
+    protected fishAnim: Animation = null;
+
+    protected _clipName: string;
+    private _baseSpeed: number;
+
+    initAssets(config: any): void {
+        if (!this.fishAnim) {
+            this.fishAnim = this.node.getChildByName('mainFish').getComponent(Animation);
+        }
+        super.initAssets(config);
+
+        for (let i = 0; i < this.fishAnim.clips.length; i++) {
+            this.fishAnim.removeState(this.fishAnim.clips[0].name);
+        }
+        this.fishAnim.clips = [config.asset];
+        this.fishAnim.play(config.asset.name);
+        this._clipName = this.fishAnim.clips[0].name;
+        this._baseSpeed = config.asset.speed;
+    }
+
+    playEffectDie(): void {
+        this.fishAnim.getState(this._clipName).speed = this._baseSpeed * 2;
+    }
+
+    resetColor(): void {
+        this.fishAnim.getComponent(Sprite).color = this.NORMAL_COLOR;
+    }
+
+    setColor(color: Color): void {
+        this.fishAnim.getComponent(Sprite).color = color;
+    }
+
+    changeAnimationSpeed(x: number = 0.5): void {
+        this.fishAnim.getState(this._clipName).speed *= x;
+    }
+
+    resetAnimationSpeed(): void {
+        if (this.fishAnim.clips.length > 0) {
+            this.fishAnim.getState(this._clipName).speed = this._baseSpeed;
+        }
+    }
+}
