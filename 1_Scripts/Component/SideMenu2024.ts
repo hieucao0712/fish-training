12d614bc00cd9958c5256b03e9d814357a348b8a
ericwalkerw <an.pham@enotion.io>
2024-03-18 10:57:54 +0700

    setup gameLayer

---
+++ 1_Scripts/Component/SideMenu2024.ts
@@ -0,0 +1,9 @@
+import { _decorator, Component, Node } from 'cc';
+import { GfSideMenu } from '../../../../cc-common/cc30-fishbase/Scripts/Common/gfSideMenu';
+const { ccclass, property } = _decorator;
+
+@ccclass('SideMenu2024')
+export class SideMenu2024 extends GfSideMenu {
+    
+}
+
