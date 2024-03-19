694d18a7ccf6f0fb84ae0a0b9cb744b78550b320
ericwalkerw <an.pham@enotion.io>
2024-03-19 11:17:14 +0700

    update asset

---
+++ 1_Scripts/Component/EffectLockFish2024.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('EffectLockFish2024')
+export class EffectLockFish2024 extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
