e7940d4fa66d83e9b41c15ac4bea131d99ae7e37
thenthen1612 <thanh.dao@enotion.io>
2024-03-19 14:09:56 +0700

    update Notify Msg

---
+++ 1_Scripts/Component/NotifyMessage2024.ts
@@ -0,0 +1,22 @@
+import { _decorator, director, tween, Tween, Vec3 } from 'cc';
+import gfDataStore from '../../../../cc-common/cc30-fishbase/Scripts/Common/gfDataStore';
+import gfBaseConfig from '../../../../cc-common/cc30-fishbase/Scripts/Config/gfBaseConfig';
+import { GfNotifyMessage } from '../../../../cc-common/cc30-fishbase/Scripts/Components/gfNotifyMessage';
+const { ccclass, property } = _decorator;
+
+@ccclass('NotifyMessage2024')
+export class NotifyMessage1998 extends GfNotifyMessage {
+
+    public onBeforeSceneChange() {
+        this._lstMessage = [];
+        this.hide();
+        super.onBeforeSceneChange();
+    }
+
+    protected onStackMessage() {
+        const position = gfBaseConfig.instance.NOTIFY_MESSAGE.position[gfDataStore.instance.getCurrentSceneName()];
+        this.node.setPosition(position.x, position.y);
+        super.onStackMessage();
+    }
+}
+
