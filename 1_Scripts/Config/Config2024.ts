ef31e72c7da75f62afd010108dac244e320fe1f8
thenthen1612 <thanh.dao@enotion.io>
2024-03-22 11:57:36 +0700

    update Scene and gameConfig

--- 1_Scripts/Config/Config2024.ts
+++ 1_Scripts/Config/Config2024.ts
@@ -7,17 +7,21 @@ export default class Config2024 extends gfBaseConfig {
     constructor() {
         super();
         Config2024.instance = this;
-        this.GameId = '1998';
+        this.GameId = '2024';
         this.GameVersion = '0.0.1';
         this.IPMasterName = 'IPMaster2';
         this.RoomVersion = '10';
+        // this.RoomKind.VIP = '3';
         this.SceneName.Lobby = 'ktfLobby2024';
         this.SceneName.Game1 = 'ktfGame2024';
+        // this.SceneName.Game2 = 'ktfDragon2024'
+        // this.LoadGameByPrefab = true;
 
         this.NOTIFY_JACKPOT.array_type_notify_jackpot = [];
         this.NOTIFY_MESSAGE.position = {
             [this.SceneName.Game1]: v3(0, 320),
             [this.SceneName.Lobby]: v3(0, 320),
+            // [this.SceneName.Game2]: v3(0, 320),
         };
 
         this.SOUND_SLIDER = true;
