c80b738b01db05eff7e9d7632b754d3ccde11573
kycaoFE <ky.cao@enotion.io>
2024-03-26 05:42:44 +0700

    add fishGroup3( Arrow popup)

--- 1_Scripts/FishGroup/FishGroup3Extend2024.ts
+++ 1_Scripts/FishGroup/FishGroup3Extend2024.ts
@@ -1,7 +1,83 @@
-import { _decorator, Component, Node } from "cc";
-import gfFishGroup3 from "../../../../cc-common/cc30-fishbase/Modules/FishGroup/Groups/gfFishGroup04";
+import { _decorator, Component, Node, v2 } from "cc";
+import FishGroupHelper from "../../../../cc-common/cc30-fishbase/Modules/FishGroup/gfFishGroupHelper";
+import GameConfig from "../../../../cc-common/cc30-fishbase/Scripts/Config/gfBaseConfig";
+import FishGroupData from "../../../../cc-common/cc30-fishbase/Modules/FishGroup/gfFishGroupData";
+import { FISH_ACTION, FishMoveActions } from "../../../../cc-common/cc30-fishbase/Modules/FishGroup/gfFishMoveActions";
 const { ccclass, property } = _decorator;
 
+const TIME_WAVE = 20;
+const SPEED_WAVE = 100;
+const PAD_X = 50;
+const PAD_Y = 25;
 @ccclass("FishGroup3Extend2024")
-export class FishGroup3Extend2024 extends gfFishGroup3 {
+export class FishGroup3Extend2024 extends Component {
+
+    static create() {
+        const {AppSize} = GameConfig.instance;
+        const startPosWave = v2(AppSize.Width/2, AppSize.Height / 2 - 500);
+        const startPosWaveFlip = v2(AppSize.Width/2, AppSize.Height / 2 + 500);
+        const startPosWaveHorizontal = v2(AppSize.Width/2 - 700, AppSize.Height / 2);
+        const startPosWaveFlipHorizontal = v2(AppSize.Width/2 + 700, AppSize.Height / 2);
+        const distanceWave = TIME_WAVE * SPEED_WAVE + AppSize.Height / 2;
+        const distanceWaveHo = TIME_WAVE * SPEED_WAVE + AppSize.Width / 2;
+
+        this.createFishVFormation(23, false,startPosWave , distanceWave, PAD_X, PAD_Y, 90);
+        this.createFishVFormation(23, true, startPosWaveFlip , distanceWave, PAD_X, PAD_Y, -90);
+
+        this.createFishVFormationHorizontal(23, false, startPosWaveHorizontal, distanceWaveHo, PAD_X, PAD_Y, 0);
+        this.createFishVFormationHorizontal(23, true, startPosWaveFlipHorizontal, distanceWaveHo, PAD_X, PAD_Y, 180);
+
+    }
+
+     static createFishVFormation(fishCount, isFlip, startPos, distanceWave, spaceX, spaceY, angle) {
+        for (let i = 0; i < fishCount; ++i) {
+            const fishInfo = FishGroupData.getNextFishData();
+            if (!fishInfo) continue;
+            let pos;
+            if (i === 0) {
+                pos = startPos;
+            } else if (i % 2 != 0) {
+                const posX = startPos.x - spaceX * Math.round(i / 2);
+                const posY = isFlip ? startPos.y + spaceY * Math.round(i / 2) : startPos.y - spaceY * Math.round(i / 2);
+                pos = v2(posX, posY);
+            } else if (i % 2 === 0) {
+                const posX = startPos.x + spaceX * Math.round(i / 2);
+                const posY = isFlip ? startPos.y + spaceY * Math.round(i / 2) : startPos.y - spaceY * Math.round(i / 2);
+                pos = v2(posX, posY);
+            }
+            this.moveFish(pos, angle, distanceWave,fishInfo);
+        }
+    }
+
+    static createFishVFormationHorizontal(fishCount, isFlip, startPos, distanceWave, spaceX, spaceY, angle) {
+        for (let i = 0; i < fishCount; ++i) {
+            const fishInfo = FishGroupData.getNextFishData();
+            if (!fishInfo) continue;
+            let pos;
+            if (i === 0) {
+                pos = startPos;
+            } else if (i % 2 != 0) {
+                const posY = startPos.y - spaceY * Math.round(i / 2);
+                const posX = isFlip ? startPos.x + spaceX * Math.round(i / 2) : startPos.x - spaceX * Math.round(i / 2);
+                pos = v2(posX, posY);
+            } else if (i % 2 === 0) {
+                const posY = startPos.y + spaceY * Math.round(i / 2);
+                const posX = isFlip ? startPos.x + spaceX * Math.round(i / 2) : startPos.x - spaceX * Math.round(i / 2);
+                pos = v2(posX, posY);
+            }
+            this.moveFish(pos, angle, distanceWave,fishInfo);
+        }
+    }
+
+    static moveFish(pos, angle, distanceWave, fishInfo){
+        const moveAction = new FishMoveActions(pos, angle);
+        moveAction.startAngle = angle;
+        moveAction.appendAction(FISH_ACTION.MoveByDistance, {
+            time: TIME_WAVE,
+            angle: angle * Math.PI / 90,
+            distance: distanceWave
+        });
+        fishInfo.moveAction = moveAction;
+        FishGroupHelper.createFishWithDelay(fishInfo);
+    }
 }
