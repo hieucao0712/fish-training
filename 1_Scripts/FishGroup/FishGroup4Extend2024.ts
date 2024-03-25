86009556d04a529ed332c93e46392c41dac29aff
ericwalkerw <an.pham@enotion.io>
2024-03-25 14:15:55 +0700

    add fish group 1

---
+++ 1_Scripts/FishGroup/FishGroup4Extend2024.ts
@@ -0,0 +1,7 @@
+import { _decorator, Component, Node } from "cc";
+import gfFishGroup4 from "../../../../cc-common/cc30-fishbase/Modules/FishGroup/Groups/gfFishGroup10";
+const { ccclass, property } = _decorator;
+
+@ccclass("FishGroup4Extend2024")
+export class FishGroup4Extend2024 extends gfFishGroup4 {
+}
