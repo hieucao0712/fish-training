e9621a205dfa01187111120a99689cbf71da985c
thenthen1612 <thanh.dao@enotion.io>
2024-03-29 11:17:02 +0700

    update crystal Ball script

--- 1_Scripts/Boss/CrystalBall.ts
+++ 1_Scripts/Boss/CrystalBall.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, tween, UIOpacity, UITransform, v3 } from 'cc';
+import { _decorator, Component, isValid, Node, tween, UIOpacity, UITransform, v3 } from 'cc';
 import GameConfig from '../Config/Config2024';
 import { isPointInScreen } from '../../../../cc-common/cc30-fishbase/Scripts/Utilities/gfUtilities';
 const { ccclass, property } = _decorator;
@@ -12,25 +12,25 @@ export class CrystalBall extends Component {
     eff1: Node;
 
     onLoad() {
-        this.node.flyGemToPlayer = this.flyGemToPlayer.bind(this);
+        this.node["flyGemToPlayer"] = this.flyGemToPlayer.bind(this);
     }
 
     flyGemToPlayer(pos, callback) {
         this.mainItem.scale = v3(0,0);
         this.eff1.scale = v3(0,0);
-        this.eff1.getComponent(UIOpacity).opacity = 0;
+        // this.eff1.getComponent(UIOpacity).opacity = 0;
         const appearTime = 0.25;
         const moveTime = 0.5;
         const standTime = 0.75;
         const dest = this.calculateMovePoint();
         const tweenEffect = tween(this.eff1)
-            .to(appearTime, { scale: 1.2, opacity: 255 });
+            .to(appearTime, { scale: v3(1.2, 1.2) });     // delete set opacity 255
         const tweenMainItem = tween(this.mainItem)
-            .to(appearTime, { scale: 2 });
+            .to(appearTime, { scale: v3(2, 2) });
         const tweenEffect2 = tween(this.eff1)
-            .to(moveTime, { scale: 0.6});
+            .to(moveTime, { scale: v3(0.6, 0.6)});
         const tweenMainItem2 = tween(this.mainItem)
-            .to(moveTime, { scale: 1 });
+            .to(moveTime, { scale: v3(1, 1) });
         tween(this.node)
             .parallel(
                 tween()
@@ -56,16 +56,16 @@ export class CrystalBall extends Component {
     }
 
     calculateMovePoint(){
-        let x = this.node.x;
-        let y = this.node.y;
+        let x = this.node.position.x;
+        let y = this.node.position.y;
 
-        let pos = v3(this.node.x, GameConfig.instance.AppSize.Height / 2);
+        let pos = v3(this.node.position.x, GameConfig.instance.AppSize.Height / 2);
 
         while(!isPointInScreen(pos)){
             pos.x = pos.x < GameConfig.instance.AppSize.Width / 2 ? pos.x + 150 : pos.x - 150;
         }
         x = pos.x;
-        pos = v3(GameConfig.instance.AppSize.Width / 2, this.node.y);
+        pos = v3(GameConfig.instance.AppSize.Width / 2, this.node.position.y);
 
         while(!isPointInScreen(pos)){
             pos.y = pos.y < GameConfig.instance.AppSize.Height / 2 ? pos.y + 150 : pos.y - 150;
