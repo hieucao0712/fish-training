fce859ba9285697432c93e17451cc1a31274cd8e
kycaoFE <ky.cao@enotion.io>
2024-03-29 08:57:32 +0700

    update plasma dragon

--- 1_Scripts/Common/MainController2024.ts
+++ 1_Scripts/Common/MainController2024.ts
@@ -1,6 +1,9 @@
 import { v2 } from 'cc';
 import gfMainController from '../../../../cc-common/cc30-fishbase/Scripts/Common/gfMainController';
 import DataStore from '../../../../cc-common/cc30-fishbase/Scripts/Common/gfDataStore';
+import NetworkParser from '../../../../cc-common/cc30-fishbase/Scripts/Network/gfNetworkParser';
+import Emitter from '../../../../cc-common/cc30-fishbase/Scripts/Common/gfEventEmitter';
+import EventCode from './EventsCode2024';
 
 export default class MainController2024 extends gfMainController {
     onUpdateEventTray(data: any): void {
@@ -13,5 +16,23 @@ export default class MainController2024 extends gfMainController {
                 pos: v2(),
             });
         }
+    }
+
+    initEvents() {
+        super.initEvents();
+        NetworkParser.instance.registerEvent(2060, this.onHitGodzilla.bind(this));
+    }
+
+    onHitGodzilla(data){
+        const {WinAmount, DeskStation, Wallet} = data;
+        if(!WinAmount){
+            const wlData = {
+                DeskStation,
+                Wallet
+            };
+            // Emitter.instance.emit(EventCode.PLAYER_LAYER.GAME_UPDATE_WALLET, wlData);
+        }
+
+        Emitter.instance.emit(EventCode.GODZILLA.ON_HIT_GODZILLA, data);
     }
 }
