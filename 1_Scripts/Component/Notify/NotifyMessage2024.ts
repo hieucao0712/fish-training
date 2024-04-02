1a3d04a1c1f8cca9ce7b52f4f2dfd87dff0e4c03
Hieu Cao <hieu.cao@enotion.io>
2024-04-02 16:24:44 +0700

    fix gold reward when shoot lion fish

--- 1_Scripts/Component/Notify/NotifyMessage2024.ts
+++ 1_Scripts/Component/Notify/NotifyMessage2024.ts
@@ -1,13 +1,17 @@
 import { _decorator } from 'cc';
 import gfDataStore from '../../../../../cc-common/cc30-fishbase/Scripts/Common/gfDataStore';
-import GameConfig from '../../Config/Config2024'
+import GameConfig from '../../Config/Config2024';
 import { GfNotifyMessage } from '../../../../../cc-common/cc30-fishbase/Scripts/Components/gfNotifyMessage';
 const { ccclass } = _decorator;
 
 @ccclass('NotifyMessage2024')
 export class NotifyMessage2024 extends GfNotifyMessage {
     protected validateData(data: any): boolean {
-        return data.type !== GameConfig.instance.NOTIFY_TYPE.MESSAGE_EVENT && super.validateData(data);
+        return (
+            GameConfig.instance.NOTIFY_CONFIG[data.type] &&
+            data.type !== GameConfig.instance.NOTIFY_TYPE.MESSAGE_EVENT &&
+            super.validateData(data)
+        );
     }
 
     public onBeforeSceneChange() {
